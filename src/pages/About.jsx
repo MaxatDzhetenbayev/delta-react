a18ffcef11b258ae565761a3ac7ee0265aa81a0f
Magzhan K <kasymmaks31@gmail.com>
2024-02-15 17:36:53 +0600

    feat: about
    created new page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+
 
 export const About = () => {
   return (
-    <div>About</div>
+    <div>
+        <li>
+            <ul>
+                Home
+            </ul>
+            <ul>
+                Contacts
+            </ul>
+            <ul>
+                About
+            </ul>
+        </li>
+        <main>
+            <h1>Delta Educatin - это лучшая школа программирования в городе Семей</h1>
+            <p>Delta Education предоставляет курсы на выбор Python-developer, PHP-developer, HTML & CSS, Базы данных, Unity-developer
+, Frontend-developer </p>
+        </main>
+        <footer>
+            <div>
+            Адрес: город Семей, улица Гагарина 32
+            </div>
+            <div>
+            График работы: пн-пт 09:00 - 18:00
+            </div>
+        </footer>
+    </div>
   )
 }
 
