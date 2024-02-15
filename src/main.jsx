150ded39078ddf4b41d17fb6602ddb450ab20ec5
MaxatDzhetenbaev <yaboku18@mail.ru>
2024-02-15 17:35:27 +0300

    ref: добавлены страницы в навигаций

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { About } from './pages/About.jsx'
+import { HomePage } from './pages/HomePage.jsx'
+import { Wrapper } from './wrapper/Wrapper.jsx'
 
 
 const router = createBrowserRouter([
   {
-    path: '/about',
-    element: <About/> 
+    element: <Wrapper/>,
+    children: [
+      {
+        path: '/about',
+        element: <About/> 
+      },
+      {
+        path: "/",
+        element: <HomePage/>
+      }
+    ]
   }
+ 
 ])
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}>
-      <App />
     </RouterProvider>
   </React.StrictMode>,
 )
